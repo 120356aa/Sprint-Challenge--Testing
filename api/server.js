35948f936165674932e92cb98d7833e98cbd7b10
120356aa <yurlovandrew@gmail.com>
2019-04-19 18:15:00 -0700

    server setup

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const games = require('../games/gamesModel.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+  res.status(200).json({ api: 'running' });
+});
+
+server.get('/games', async (req, res) => {
+  const rows = await games.getAll();
+  res.status(200).json(rows);
+});
+
+module.exports = server;
