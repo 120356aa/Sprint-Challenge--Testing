276727b98e60ab0ed9b3dfb3e71b649e58749885
120356aa <yurlovandrew@gmail.com>
2019-04-19 19:01:15 -0700

    MVP

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,6 +5,8 @@ const Game = require('../games/gamesModel.js');
 const db = require('../data/dbConfig.js');
 
 describe('GET /games', () => {
+  beforeEach(() => db('games').truncate());
+
   it('should return status 200', async () => {
     const res = await request(server).get('/games');
     expect(res.status).toEqual(200);
@@ -18,5 +20,23 @@ describe('GET /games', () => {
   it('should return an array', async () => {
     const res = await request(server).get('/games');
     expect(res.body).toEqual([]);
+  });
+});
+
+describe('insert game test', () => {
+  beforeEach(() => db('games').truncate());
+
+  it('insert game return length of 1', async () => {
+    await Game.insert({ title: 'COD', genre: 'FPS', releaseYear: '2000' });
+    const game = await db('games');
+
+    expect(game.length).toBe(1);
+  });
+
+  it('insert game return game to be COD', async () => {
+    await Game.insert({ title: 'COD', genre: 'FPS', releaseYear: '2000' });
+    const game = await db('games');
+
+    expect(game[0].title).toBe('COD');
   });
 });
